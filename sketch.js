7156a6238b4e4fe976910757e4dc30c12fb8da1e
almirAlmir <aasc@cin.ufpe.br>
2023-05-08 02:34:43 -0300

    Resolvendo problema de indexar o array de curvas etc

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
-let curves = [];
+window.addEventListener("features", (Event) => Event.preventDefault());
+let curves;
 let width = 800;
 let height = 400;
 let curve = 0;
+let Evaluation = [5, 10, 20, 80];
+let evalIndex = 3;
 
 //-----------------------------
 //UI
@@ -19,7 +22,7 @@ function setup() {
 
   curves = [
     {
-      index: 0,
+      //index: 0,
       color: [0, 0, 255], // redish
       bezier: [],
       points: [],
@@ -30,30 +33,44 @@ function setup() {
   
   buttonAdd = createButton("ADD");
   buttonDel = createButton("DEL");
-  buttonNEvaluations = createButton("Nº Eval");
+  buttonNEvaluations = createButton("Nº Eval"+ Evaluation[evalIndex]);
   buttonSelectcurve = createButton("Select");
   checkDot = createCheckbox("hide Dots", false);
   checkPoligon = createCheckbox("hide Poligons", false);
   checkCurves = createCheckbox("hide Curves", false);
   
   selectBox = createSelect();
-  selectBox.option("1");
-  selectBox.option("2");
-  selectBox.option("3");
-  selectBox.option("4");
-  selectBox.option("5");
-  selectBox.option("6");
-  selectBox.option("7");
-  selectBox.option("8");
-  selectBox.option("9");
-  selectBox.option("10");
+  selectBox.option(1);
+  selectBox.option(2);
+  selectBox.option(3);
+  selectBox.option(4);
+  selectBox.option(5);
+  selectBox.option(6);
+  selectBox.option(7);
+  selectBox.option(8);
+  selectBox.option(9);
+  selectBox.option(10);
   //Actions
   buttonAdd.mouseClicked(() => {
     CurveAdd();
+    curve = curves.length-1;
+
   });
 
   buttonSelectcurve.mouseClicked(() => {
-    selectCurve(selectBox.value-1);
+    curve = selectBox.value()-1;
+  })
+
+  buttonDel.mouseClicked(() => {
+    curves.splice(curve, 1);
+    if(curves.length <= 1){
+      CurveAdd();
+    }
+    
+  })
+
+  buttonNEvaluations.mouseClicked(() => {
+    evalIndex = (evalIndex+1) % (4);
   })
 
 }
@@ -69,7 +86,7 @@ function draw() {
   stroke(200, 150, 200);
   strokeWeight(3);
   line(40, 40, 197, 280);*/
-
+  addBezier();
   for(let i = 0; i <= curves.length-1; i++){
 
     for(let j = 0; j<=curves[i].points.length-1; j++){
@@ -83,11 +100,9 @@ function draw() {
       strokeWeight(5);
       line(curves[i].points[j].x, curves[i].points[j].y, curves[i].points[j+1].x, curves[i].points[j+1].y);
     }
-    addBezier();
-    
+    stroke(curves[i].color[0]-30, curves[i].color[1]-30, curves[i].color[2]-30);
+    strokeWeight(5);
     for(let j = 0; j<=curves[i].points.length-2; j++){
-      stroke(curves[i].color[0]-30, curves[i].color[1]-30, curves[i].color[2]-30);
-      strokeWeight(5);
       line(curves[i].bezier[j].x, curves[i].bezier[j].y, curves[i].bezier[j+1].x, curves[i].bezier[j+1].y);
     }
 
@@ -103,31 +118,31 @@ function interpolate(t, p0, p1) {
   return pResult;
 }
 function deCasteljau(points, nEvaluations) {
-  if (points.length < 1) return [];
-  result = [];
-  start = points[0];
-  for (let t = 0; t <= 1; t += 1 / nEvaluations) {
-    controls = points;
+  if (points === undefined || points.length < 1) return [];
+        result = [];
+        start = points[0];
+        for (let t = 0; t <= 1; t += 1 / nEvaluations) {
+          controls = points;
 
-    while (controls.length > 1) {
-      aux = [];
+          while (controls.length > 1) {
+            aux = [];
 
-      for (i = 0; i < controls.length - 1; i++) {
-        aux[i] = interpolate(t, controls[i], controls[i + 1]);
-      }
-      controls = aux;
-    }
+            for (i = 0; i < controls.length - 1; i++) {
+              aux[i] = interpolate(t, controls[i], controls[i + 1]);
+            }
+            controls = aux;
+          }
 
-    result.push(controls[0]);
-  }
-  return result;
+          result.push(controls[0]);
+        }
+        return result;
 }
 function addBezier(){
 
   for (let i = 0; i < curves.length; i++) {
     curves[i].bezier = deCasteljau(
       curves[i].points,
-      80
+      Evaluation[evalIndex]
     );
   }
 
@@ -137,24 +152,26 @@ function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
 }
 //Project implementation
-window.addEventListener("features", (Event) => Event.preventDefault());
+
 
 function mousePressed(){
 
-  /*pointSelected = false;
+  if(mouseX > width || mouseY > height || mouseY < height-400) return;
+
+  pointSelected = false;
   for(j = 0; j<curves[curve].points.length ; j++){
 
     if(dist(mouseX, mouseY,
-      curves[curve].points[j].x, curves[curve].points[j].y) < 2){
+      curves[curve].points[j].x, curves[curve].points[j].y) <= 20){
 
         pointSelected = true;
       }
   }
 
-  if(pointSelected) return;*/
+  if(pointSelected) return;
+
   const newpoint = new Point(mouseX, mouseY);
   curves[curve].points.push(newpoint);
-
 
 }
 
@@ -179,6 +196,10 @@ function doubleClicked(){
   }
 }
 
+function mouseReleased(){
+  return;
+}
+
 function mouseDragged(){
 
   pointSelected = false;
@@ -186,7 +207,7 @@ function mouseDragged(){
   for(let j = 0; j<curves[curve].points.length ; j++){
 
     if(dist(mouseX, mouseY,
-      curves[curve].points[j].x, curve.points[j].y) <= 20){
+      curves[curve].points[j].x, curves[curve].points[j].y) <= 20){
 
         pointSelected = true;
         moveIndex = j;
@@ -207,7 +228,7 @@ function selectCurve(curveIndex){
 function CurveAdd(){
   
   const newcurve = {
-    index : indexCurve,
+   //index : indexCurve,
     color : [getRandomArbitrary(1, 255), getRandomArbitrary(1, 255), getRandomArbitrary(1, 255)],
     bezier : [],
     points : [],
